eec2f1fa3464176f8e018525d4d83fec26e228b1
Dan Čermák <dcermak@suse.com>
2025-06-26 23:59:48 +0200

    Increase tukit verbosity

--- container-snap-new-kernel.sh
+++ container-snap-new-kernel.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-tukit call "$1" zypper -n in -f grub2
-tukit call "$1" dracut --force --regenerate-all
-tukit call "$1" bash -c "/usr/sbin/grub2-mkconfig > /boot/grub2/grub.cfg"
-tukit call "$1" /sbin/pbl --install
+tukit -v call "$1" zypper -n in -f grub2
+tukit -v call "$1" dracut --force --regenerate-all
+tukit -v call "$1" bash -c "/usr/sbin/grub2-mkconfig > /boot/grub2/grub.cfg"
+tukit -v call "$1" /sbin/pbl --install
