1a0130656610f898b0539ba52195b7b8b9879e90
Dan Čermák <dcermak@suse.com>
2025-06-26 07:20:45 +0200

    Add a kiwi description to build a container-snap enabled image

---
+++ images/config.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+suseSetupProduct
+
+#======================================
+# Specify default systemd target
+#--------------------------------------
+baseSetRunlevel 3
+
+#======================================
+# Set hostname by DHCP
+#--------------------------------------
+baseUpdateSysConfig /etc/sysconfig/network/dhcp DHCLIENT_SET_HOSTNAME yes
+
+#======================================
+# Enable DHCP on eth0
+#--------------------------------------
+cat >/etc/sysconfig/network/ifcfg-eth0 <<EOF
+BOOTPROTO='dhcp'
+STARTMODE='auto'
+EOF
+
+# Add repos from /etc/YaST2/control.xml
+if [ -x /usr/sbin/add-yast-repos ]; then
+	add-yast-repos
+	zypper --non-interactive rm -u live-add-yast-repos
+fi
+
+# import rpm keys, this is not done automatically for some reason
+for key in /usr/lib/rpm/gnupg/keys/*; do
+    rpm -import "$key"
+done
+
+systemctl enable NetworkManager.service
+systemctl enable systemd-resolved.service
+
+# Enable container-snap-first-boot service
+systemctl enable container-snap-first-boot.service
+
+# Create the first-boot-required file
+mkdir -p /var/lib/container-snap
+touch /var/lib/container-snap/.first-boot-required
